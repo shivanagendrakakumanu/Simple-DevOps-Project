5528b8bd1692bda7dd9c42cfdbafc5193f49322a
KakumanuShiva.Nagnedra <kakumanushiva.nagnedra@thomsonreuters.com>
2022-03-06 17:29:09 +0530

    X/bash: create

---
+++ X/bash.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# echo "---------"
+# read -p "Enter two numbers to add" num1 num2
+# function add()
+# {
+#     echo "$(($num1 + $num2))"
+# }
+# add $num1 $num2
+# echo "---------"
+# read -p "Enter a number two print n numbers" numbers
+# for (( i=1; i<=numbers; i++ ))
+# do
+#     echo $i
+# done
+# echo "---------"
+# read -p "sum of n numbers :" silo
+# sumOfNumber=silo
+# i=0
+# while [ $i -lt $silo ]
+# do 
+#     sumOfNumber=$((sumOfNumber + i))
+#     i=$((i+1))
+# done
+# echo "$sumOfNumber"
+# echo "---------"
+# echo "Enter a number"
+# read NUMBER
+# if [ $NUMBER -le 5 ]
+# then
+#     echo "$NUMBER is less than 5"
+# else
+#     echo "Greater than 5"
+# fi
+# echo "---------"
+# read -p "enter two numbers to add" var1 var2
+# # echo $((var1 + var2)) or 
+# echo `expr $var1 + $var2`
+# echo "---------"
+# echo "a number to get power of 2"
+# read number
+# echo $((2**$number))
+# echo "---------"
+#passing positional arguments
+# echo $1 $2
+# echo "---------"
+# var="Hello World"
+# echo $var
+# echo "---------"
